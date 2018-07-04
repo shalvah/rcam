7e9fd8ae6fdcc0fb830af8416d5c485e76ed7229
shalvah <diakon.ng@gmail.com>
2018-07-04 22:43:14 +0100

    implement comment posting and moderation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,5 +57,45 @@ be on your merry way. Wait, did I tell you this is a dummy post?
     </div>
 </div>
 
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script>
+    let addComment = function (event) {
+        function displayComment(data) {
+            let $comment = $('<div>').text(data['text']).prepend($('<small>').html(data['username'] + "<br>"));
+            $('#comments').prepend($comment);
+        }
+
+        function showAlert(message) {
+            let $alert = $('#alert');
+            $alert.text(message).show();
+            setTimeout(() => $alert.hide(), 4000);
+        }
+
+        event.preventDefault();
+        let data = {
+            text: $('#text').val(),
+            username: $('#username').val(),
+        };
+        fetch('/comments', {
+            body: JSON.stringify(data),
+            credentials: 'same-origin',
+            headers: {
+                'content-type': 'application/json',
+                'x-csrf-token': $('meta[name="csrf-token"]').attr('content')
+            },
+            method: 'POST',
+            mode: 'cors',
+        }).then(response => {
+            if (response.ok) {
+                displayComment(data);
+            } else {
+                showAlert('Your comment was not approved for posting. Please be nicer :)');
+            }
+        })
+    }
+</script>
 </body>
 </html>
